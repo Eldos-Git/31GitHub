af0c68fffd2d2b87d9e58ab5dee75580b9ff9c7d
Eldos-Git <68679528+Eldos-Git@users.noreply.github.com>
2023-12-09 20:58:25 +0600

    Create Day 21 - 101. Symmetric Tree.py

---
+++ Day 21 - 101. Symmetric Tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def isSame(root1, root2):
+            if not root1 and not root2:
+                return True
+            if (not root1 and root2) or (root1 and not root2) or (root1.val != root2.val):
+                return False
+            left = isSame(root1.left, root2.right)
+            right = isSame(root1.right, root2.left)
+            return left and right
+        
+        if not root:
+            return True
+            
+        return isSame(root.left, root.right)
